f61f4be97597fd682157160ca7b7f88f48a54272
Robert Ross <robert@creativequeries.com>
2013-12-30 16:56:25 -0800

    Initial Commit

---
+++ OHHttpStubsFailure/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OHHttpStubsFailure
+//
+//  Created by Robert Ross on 12/30/13.
+//  Copyright (c) 2013 Creative Queries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OHHttpStubsFailureTests/OHHttpStubsFailureTests.m
@@ -0,0 +1,34 @@
+//
+//  OHHttpStubsFailureTests.m
+//  OHHttpStubsFailureTests
+//
+//  Created by Robert Ross on 12/30/13.
+//  Copyright (c) 2013 Creative Queries. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OHHttpStubsFailureTests : XCTestCase
+
+@end
+
+@implementation OHHttpStubsFailureTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
